cf39f92cb5cbaed5a756752ffa947a60d46f5ff9
YAMAGAMI Satoshi <yamagami.satoshi@e-grid.co.jp>
2025-04-25 18:56:45 +0900

    temp:パスの抽出メソッド追加

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -47,4 +47,20 @@ impl Url {
             url_parts[0].to_string()
         }
     }
+
+    fn extract_path(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+        
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+        parth_and_searchpart[0].to_string()
+    }
 }
