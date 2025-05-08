63d42ac4a020d5266f9c49d3a808af0bbd74daa0
YAMAGAMI Satoshi <yamagami.satoshi@e-grid.co.jp>
2025-05-08 10:46:04 +0900

    temp:httpクライアントのgetメソッドに名前解決の処理を作成

---
+++ net/wasabi/src/http.rs
@@ -0,0 +1,29 @@
+extern crate alloc;
+use alloc::format;
+use alloc::string::String;
+use crate::alloc::string::ToString;
+use saba_core::error::Error;
+use saba_core::http::HttpResponse;
+use noli::net::lookup_host;
+
+pub struct HttpClient {}
+
+impl HttpClient {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+        let ips = match lookup_host(&host) {
+            Ok(ips) => ips,
+            Err(e) => {
+                return Err(Error::Network(format!("Failed to find IP addresses: {:#?}", e)))
+            }
+        };
+
+        if ips.len() < 1 {
+            return Err(Error::Network("Failed to find IP addresses".to_string()));
+        }
+    }
+}
+
