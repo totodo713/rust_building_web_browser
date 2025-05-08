542ad0d3ed14dbbf07bcec36f4c9e1d71ff36af4
YAMAGAMI Satoshi <yamagami.satoshi@e-grid.co.jp>
2025-05-08 11:41:40 +0900

    temp:HTTPレスポンス構造体を作成中, ヘッダー構造体を作成

---
+++ saba_core/src/http.rs
@@ -0,0 +1,29 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone)]
+pub struct HttpResponse {
+    version: String,
+    status_code: u32,
+    reason: String,
+    headers: Vec<Header>,
+    body: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct Header {
+    name: String,
+    value: String,
+}
+
+impl Header {
+    pub fn new(name: String, value: String) -> Self {
+        Self {name, value }
+    }
+}
+
+impl HttpResponse {
+    pub fn new(raw_response: String) -> Result<Self, Error> {
+    }
+}
+
--- saba_core/src/lib.rs
+++ saba_core/src/lib.rs
@@ -2,5 +2,6 @@
 
 extern crate alloc;
 
+pub mod http;
 pub mod url;
 
