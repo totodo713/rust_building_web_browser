a797be953179fae2bdc93f9af4f6e4d4aed875b7
yadoya <s.yamagami.666@gmail.com>
2025-05-10 14:37:52 +0900

    temp:ステータスラインの分割処理を追加

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -26,6 +26,15 @@ impl Header {
 impl HttpResponse {
     pub fn new(raw_response: String) -> Result<Self, Error> {
         let preprocessed_response = raw_response.trim_start().replace("\r\n", "\n");
-    }
+
+        let (status_line, remaining) = match preprocessed_response.split_once('\n') {
+            Some((s,r)) => (s,r),
+            None => {
+                return Err(Error::Network(format!(
+                            "invalid http response: {}",
+                            preprocessed_response)))
+            }
+        }
+    };
 }
 
