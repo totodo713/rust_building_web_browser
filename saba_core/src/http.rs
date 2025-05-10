b4a628cedae463eb4249389ed225df91b932b827
yadoya <s.yamagami.666@gmail.com>
2025-05-10 14:30:25 +0900

    temp:改行コードの誤りを修正

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -25,7 +25,7 @@ impl Header {
 
 impl HttpResponse {
     pub fn new(raw_response: String) -> Result<Self, Error> {
-        let preprocessed_response = raw_response.trim_start().replace("\n\r", "\n");
+        let preprocessed_response = raw_response.trim_start().replace("\r\n", "\n");
     }
 }
 
